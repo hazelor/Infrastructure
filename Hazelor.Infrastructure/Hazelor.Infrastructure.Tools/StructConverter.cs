6da6261965d29f05cfad28a2cb62410f8f2b34f9
hazelnutssoft <guoxiao@buaa.edu.cn>
2015-11-02 08:47:35 +0800

    添加 MD5,CPUType等工具

---
+++ Hazelor.Infrastructure/Hazelor.Infrastructure.Tools/StructConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazelor.Infrastructure.Tools
+{
+    public static class StructConverter
+    {
+        public static byte[] StructToBytes(object obj)
+        {
+            int rawsize = Marshal.SizeOf(obj);
+            IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+            Marshal.StructureToPtr(obj, buffer, false);
+            byte[] rawdatas = new byte[rawsize];
+            Marshal.Copy(buffer, rawdatas, 0, rawsize);
+            Marshal.FreeHGlobal(buffer);
+            return rawdatas;
+        }
+
+        public static object BytesToStruct(byte[] buf, int len, Type type)
+        {
+            object rtn;
+            IntPtr buffer = Marshal.AllocHGlobal(len);
+            Marshal.Copy(buf, 0, buffer, len);
+            rtn = Marshal.PtrToStructure(buffer, type);
+            Marshal.FreeHGlobal(buffer);
+            return rtn;
+        }
+
+        public static void BytesToStruct(byte[] buf, int len, object rtn)
+        {
+            IntPtr buffer = Marshal.AllocHGlobal(len);
+            Marshal.Copy(buf, 0, buffer, len);
+            Marshal.PtrToStructure(buffer, rtn);
+            Marshal.FreeHGlobal(buffer);
+        }
+
+        public static void BytesToStruct(byte[] buf, object rtn)
+        {
+            BytesToStruct(buf, buf.Length, rtn);
+        }
+
+        public object BytesToStruct(byte[] buf, Type type)
+        {
+            return BytesToStruct(buf, buf.Length, type);
+        }
+    }
+}
